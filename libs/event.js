f281f5bdf65fdb2c24f9d79e22ef9a6ae94170ac
swenhu <swenhu@tencent.com>
2018-04-04 20:13:49 +0800

    添加数据传输回调

--- libs/event.js
+++ libs/event.js
@@ -1,32 +1,39 @@
 class Event{
-	constructor(){
-		this.__event = {};
-	}
-	on(name, cb){
-		if(!name || typeof cb != 'function'){
-			console.error('订阅事件失败');
-			return ;
-		}
-		(this.__event[name] = this.__event[name] || []).push(cb);
-	}
-	emit(name, ...params){
-		let cache = this.__event[name];
-		if(!cache || cache.length == 0){
-			console.error(`${name}事件未订阅`);
-			return;
-		}
-		cache.forEach((item) => {
-			item.call(null, ...params);
-		})
-	}
-	off(name){
-		if(!name){
-			console.info('删除所有订阅事件！！！');
-			this.__event = {};
-			return;
-		}
-		delete this.__event[name]
-		return;
-	}
+    constructor(){
+        this.__event = {};
+    }
+    on(name, cb, data){
+        if(!name || typeof cb != 'function'){
+            console.error('订阅事件失败');
+            return ;
+        }
+        if(this.__event[name] && this.__event[name].list){
+            this.__event[name].list.push(cb)
+        }else{
+            this.__event[name] = {};
+            this.__event[name].list = [cb]
+        }
+        this.__event[name].data = data || '';
+    }
+    emit(name, ...params){
+        let cache = this.__event[name];
+        if(!cache || cache.length == 0){
+            console.error(`${name}事件未订阅`);
+            return;
+        }
+        cache.list.forEach((item) => {
+            item.call(null, ...params);
+        });
+        return cache.data
+    }
+    off(name){
+        if(!name){
+            console.info('删除所有订阅事件！！！');
+            this.__event = {};
+            return;
+        }
+        delete this.__event[name]
+        return;
+    }
 }
 export default Event;
